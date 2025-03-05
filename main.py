9e8edb0664d5964dcb66d81b138b49a44b01e6a4
Nik <nikita.buharinov@mail.ru>
2025-03-05 19:38:34 +0300

    3

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ import pandas as pd
 import numpy as np
 
 df_income = pd.read_excel('доходы.xlsx')
-df_expense = pd.read_excel('расходы.xlsx')
+df_expenses = pd.read_excel('расходы.xlsx')
 total_income = 0
-total_expense = 0
+total_expenses = 0
+total_variable_expenses = 0
 
 for i in df_income['Сумма']: # берёт каждое значение из столбца и складывает
   total_income += i
-print(f'Полная сумма доходов равна: {total_income}')
+print(f'Выручка равна: {total_income}')
 
-for i, j in zip(df_expense['Сумма'], df_expense['Категория']): # берём для каждой строки значения обоих столбцов и делаем из них массив, и присваиваем i, j значения в массиве
+for i, j in zip(df_expenses['Сумма'], df_expenses['Категория']): # берём для каждой строки значения обоих столбцов и делаем из них массив, и присваиваем i, j значения в массиве
+  total_expenses += i
   if j in ['Сырье и материалы', 'Зарплаты', 'Логистика']:
-    total_expense += i
-print(f'Полная сумма переменных расходов равна: {total_expense}')
+    total_variable_expenses += i
+print(f'Cумма всех расходов равна: {total_expenses}')
+print(f'Cумма переменных расходов равна: {total_variable_expenses}')
+
+# Маржа = Выручка − Переменные расходы
+# Маржинальность = Маржа / Выручка × 100%
+margin = total_income - total_variable_expenses
+marginality = f'{round(margin / total_income * 100, 4)}%'
+print(f'Маржа: {margin}')
+print(f'Маржинальность: {marginality}')
